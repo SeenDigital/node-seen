b04fc77f49a9c9900359094bfc16707491dae4c6
brad <bwalker8038@gmail.com>
2014-01-08 11:23:12 -0500

    adding register wrapper

--- test/a.js
+++
@@ -1,8 +0,0 @@
-var Gallery = require('../lib/gallery');
-
-
-var gallery = new Gallery('hostname', 'demo.vscampaign.com');
-
-gallery.on('gallery', function (gallery) {
-  console.log(gallery);
-});
--- test/gallery.js
+++ test/gallery.js
@@ -2,22 +2,25 @@ var assert = require('assert');
 var Gallery = require('../lib/gallery');
 
 
-describe('Retrieve Gallery', function () {
-  it('should retrieve a json object', function () {
-    var gallery = new Gallery('id', '16447811');
+describe('Retrieve Gallery from hostname', function () {
+  it('should retrieve a json object with the correct id', function () {
+    var gallery = new Gallery('hostname', 'demo.vscampaign.com');
 
     gallery.on('campaign', function (campaign) {
       assert(true, typeof(gallery) === 'Object');
+      assert(true, gallery.id === '24962110');
     });
   });
 });
 
-describe('Gallery Data is Valid', function () {
-  it('should Retrieve a gallery with valid data', function () {
-    var gallery = new Gallery('16447811');
+
+describe('Retrieve Gallery from id', function () {
+  it('should retrieve a json object with the correct id', function () {
+    var gallery = new Gallery('id', '24962110');
 
     gallery.on('campaign', function (campaign) {
-      assert(true, gallery.id === '16447811');
+      assert(true, typeof(gallery) === 'Object');
+      assert(true, gallery.id === '24962110');
     });
   });
 });
