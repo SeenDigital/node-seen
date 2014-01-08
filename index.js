c9a1ead7166958e39f42c395ab3fc6a74cfb7c64
brad <bwalker8038@gmail.com>
2014-01-08 18:35:18 -0500

    fixing missing lib

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.Gallery = require('./lib/gallery');
 exports.Campaign = require('./lib/campaign');
 exports.Entries = require('./lib/entries');
 exports.Entry = require('./lib/entry');
+exports.Register = require('./lib/register');
