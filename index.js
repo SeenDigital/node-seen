4ddbec9a72192aac64f53a62d6b1005135b97e77
brad <bwalker8038@gmail.com>
2014-03-19 15:00:02 -0400

    adding config method

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.Campaign = require('./lib/campaign');
 exports.Entries = require('./lib/entries');
 exports.Entry = require('./lib/entry');
 exports.Register = require('./lib/register');
+exports.config = require('./lib/api').config;
